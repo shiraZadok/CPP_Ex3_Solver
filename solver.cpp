ff3feb01fce4e1fcae2501ee95c2cc8591ce2607
shiraZadok <58064644+shiraZadok@users.noreply.github.com>
2020-05-06 19:05:52 +0300

    Add files via upload

--- solver.cpp
+++ solver.cpp
@@ -1,55 +1,97 @@
 //
 // Created by shira on 28/04/2020.
 //
-#include <iostream>
-//#include <complex>
 #include "solver.hpp"
-//using namespace std;
 using namespace solver;
+using namespace std;
 
-RealVariable& solver::operator+ (double d, RealVariable& r){return r;}
-RealVariable& solver::operator+ (RealVariable& r, double d){return r;}
-RealVariable& solver::operator+ (RealVariable& r1, RealVariable& r2){return r1;}
-RealVariable& solver::operator- (double d, RealVariable& r){return r;}
-RealVariable& solver::operator- (RealVariable& r, double d){return r;}
-RealVariable& solver::operator- (RealVariable& r1, RealVariable& r2){return r1;}
-RealVariable& solver::operator/ (double d, RealVariable& r){return r;}
-RealVariable& solver::operator/ (RealVariable& r, double d){return r;}
-RealVariable& solver::operator/ (RealVariable& r1, RealVariable& r2){return r1;}
-RealVariable& solver::operator* (double d, RealVariable& r){return r;}
-RealVariable& solver::operator* (RealVariable& r, double d){return r;}
-RealVariable& solver::operator* (RealVariable& r1, RealVariable& r2){return r1;}
-RealVariable& solver::operator== (double d, RealVariable& r){return r;}
-RealVariable& solver::operator== (RealVariable& r, double d){return r;}
-RealVariable& solver::operator== (RealVariable& r1, RealVariable& r2){return r1;}
-RealVariable& solver::operator^ (RealVariable& r, double d){return r;}
+RealVariable solver::operator+ (const RealVariable& r1, const RealVariable& r2){
+    return RealVariable(
+            (r1._a!=0 && r2._a!=0) ?r1._a+r2._a: (r1._a!=0 ? r1._a:r2._a),
+            (r1._b!=0 && r2._b!=0) ?r1._b+r2._b:(r1._b!=0 ? r1._b:r2._b),
+            r1._c+r2._c);
+}
 
-ComplexVariable& solver::operator+ (ComplexVariable& c, double d){return c;}
-ComplexVariable& solver::operator+ (double d, ComplexVariable& c){return c;}
-ComplexVariable& solver::operator+ (ComplexVariable& c1, complex<double> c2){return c1;}
-ComplexVariable& solver::operator+ (complex<double> c1, ComplexVariable& c2){return c2;}
-ComplexVariable& solver::operator+ (ComplexVariable& c1, ComplexVariable& c2){return c1;}
-ComplexVariable& solver::operator^ (ComplexVariable& c, double d){return c;}
-ComplexVariable& solver::operator== (double d, ComplexVariable& c){return c;}
-ComplexVariable& solver::operator== (ComplexVariable& c, double d){return c;}
-ComplexVariable& operator== (ComplexVariable& c1, complex<double> c2) {return c1;}
-ComplexVariable& operator== (complex<double> c1, ComplexVariable& c2){return c2;}
-ComplexVariable& solver::operator== (ComplexVariable& c1, ComplexVariable& c2){return c1;}
-ComplexVariable& solver::operator* (double d, ComplexVariable& c){return c;}
-ComplexVariable& solver::operator* (ComplexVariable& c, double d){return c;}
-ComplexVariable& solver::operator* (ComplexVariable& c1, complex<double> c2){return c1;}
-ComplexVariable& solver::operator* (complex<double> c1, ComplexVariable& c2){return c2;}
-ComplexVariable& solver::operator* (ComplexVariable& c1, ComplexVariable& c2){return c1;}
-ComplexVariable& solver::operator/ (double d, ComplexVariable& c){return c;}
-ComplexVariable& solver::operator/ (ComplexVariable& c, double d){return c;}
-ComplexVariable& solver::operator/ (ComplexVariable& c1, ComplexVariable& c2){return c1;}
-ComplexVariable& solver::operator/ (ComplexVariable& c1, complex<double> c2){return c1;}
-ComplexVariable& solver::operator/ (complex<double> c1, ComplexVariable& c2){return c2;}
-ComplexVariable& solver::operator- (double d, ComplexVariable& c){return c;}
-ComplexVariable& solver::operator- (ComplexVariable& c, double d){return c;}
-ComplexVariable& solver::operator- (ComplexVariable& c1, ComplexVariable& c2){return c1;}
-ComplexVariable& solver::operator- (ComplexVariable& c1, complex<double> c2){return c1;}
-ComplexVariable& solver::operator- (complex<double> c1, ComplexVariable& c2){return c2;}
+RealVariable solver::operator- (const RealVariable& r1,const RealVariable& r2){
+    return RealVariable(
+            (r1._a!=0 && r2._a!=0) ?r1._a-r2._a: (r1._a!=0 ? r1._a:-r2._a),
+            (r1._b!=0 && r2._b!=0) ?r1._b-r2._b:(r1._b!=0 ? r1._b:-r2._b),
+            r1._c-r2._c);
+}
 
-double solver::solve(RealVariable r){return 0;}
-complex<double> solver::solve(ComplexVariable c){return 3;}
+RealVariable solver::operator/ (const RealVariable& r1, const RealVariable& r2){
+    if(r2._a==0 && r2._c==0 && r2._b==0) throw runtime_error("Must not divide by 0");
+    return RealVariable(r1._a/r2._c,r1._b/r2._c,r1._c/r2._c);
+}
+
+RealVariable solver::operator* (const RealVariable& r1, const RealVariable& r2){
+    if((r1._a!=0&&(r2._a!=0||r2._b!=0)) || (r2._a!=0&&(r1._a!=0||r1._b!=0)))
+        throw runtime_error("ERROR: The power of the quadratic equation are more then 2");
+    if (r1._a==0&&r1._b==0)
+        return RealVariable(r2._a*r1._c,r2._b*r1._c,r2._c*r1._c);
+    if (r2._a==0&&r2._b==0)
+        return RealVariable(r1._a*r2._c,r1._b*r2._c,r1._c*r2._c);
+    return RealVariable(r1._b*r2._b,r1._b*r2._c+r1._c*r2._b,r1._c*r2._c);
+}
+
+RealVariable solver::operator^ (const RealVariable& r, double d){
+    if((d>2 && r._b!=0) || (d>=2&&r._a!=0)|| d==0)
+        throw runtime_error("ERROR: The power of the quadratic equation are more then 2");
+    return RealVariable(pow(r._b,d),(2*r._b*r._c),pow(r._c,2));
+}
+
+//ComplexVariable solver::operator+ (ComplexVariable& c1, complex<double> c2){
+//    return ComplexVariable(c1._a,c1._b,c1._c+c2);}
+
+ComplexVariable solver::operator+ (const ComplexVariable& c1, const ComplexVariable& c2){
+    return ComplexVariable(
+            (c1._a!=ZERO_COMPLEX && c2._a!=ZERO_COMPLEX) ?c1._a+c2._a: (c1._a!=ZERO_COMPLEX ? c1._a:c2._a),
+            (c1._b!=ZERO_COMPLEX && c2._b!=ZERO_COMPLEX) ?c1._b+c2._b:(c1._b!=ZERO_COMPLEX ? c1._b:c2._b),
+            c1._c+c2._c);
+}
+ComplexVariable solver::operator^ (const ComplexVariable& c, double d){
+    if((d>2 && c._b!=ZERO_COMPLEX) || (d>=2&&c._a!=ZERO_COMPLEX) || d==0)
+        throw runtime_error("ERROR: The power of the quadratic equation are more then 2");
+    return ComplexVariable(pow(c._b,d),(2.0*c._b*c._c),pow(c._c,2));
+}
+
+ComplexVariable solver::operator* (const ComplexVariable& c1, const ComplexVariable& c2){
+    if((c1._a!=ZERO_COMPLEX&&(c2._a!=ZERO_COMPLEX||c2._b!=ZERO_COMPLEX)) ||
+       (c2._a!=ZERO_COMPLEX&&(c1._a!=ZERO_COMPLEX||c1._b!=ZERO_COMPLEX)))
+        throw runtime_error("ERROR: The power of the quadratic equation are more then 2");
+    if (c1._a==ZERO_COMPLEX&&c1._b==ZERO_COMPLEX)
+        return ComplexVariable(c2._a*c1._c,c2._b*c1._c,c2._c*c1._c);
+    if (c2._a==ZERO_COMPLEX&&c2._b==ZERO_COMPLEX)
+        return ComplexVariable(c1._a*c2._c,c1._b*c2._c,c1._c*c2._c);
+    return ComplexVariable(c1._b*c2._b,c1._b*c2._c+c1._c*c2._b,c1._c*c2._c);
+}
+
+ComplexVariable solver::operator/ (const ComplexVariable& c1, const ComplexVariable& c2){
+    if(c2._a==ZERO_COMPLEX && c2._c==ZERO_COMPLEX && c2._b==ZERO_COMPLEX) throw runtime_error("Must not divide by 0");
+    return ComplexVariable(c1._a/c2._c,c1._b/c2._c,c1._c/c2._c);
+}
+
+ComplexVariable solver::operator- (const ComplexVariable& c1, const ComplexVariable& c2){
+    return ComplexVariable(
+            (c1._a!=ZERO_COMPLEX && c2._a!=ZERO_COMPLEX) ?c1._a-c2._a: (c1._a!=ZERO_COMPLEX ? c1._a:-c2._a),
+            (c1._b!=ZERO_COMPLEX && c2._b!=ZERO_COMPLEX) ?c1._b-c2._b:(c1._b!=ZERO_COMPLEX ? c1._b:-c2._b),
+            c1._c-c2._c);
+}
+
+double solver::solve(RealVariable r) {
+    if (r._a==0&&r._b==0) throw runtime_error("ERROR: There is no solution");
+    if (r._a != 0){
+        double discriminant = pow(r._b, 2) - 4 * r._a * r._c;
+        if (discriminant < 0) throw runtime_error("ERROR: There is no real solution");
+        else return (-r._b + sqrt(discriminant)) / (2 * r._a);
+    }
+    else return (-r._c)/r._b;
+}
+complex<double> solver::solve(ComplexVariable c){
+    if (c._a==ZERO_COMPLEX&&c._b==ZERO_COMPLEX) throw runtime_error("ERROR: There is no solution");
+    if (c._a != ZERO_COMPLEX){
+        complex<double> discriminant = sqrt(pow(c._b, 2) - 4.0 * c._a * c._c);
+        return (-c._b + discriminant) / (2.0 * c._a);
+    }
+    else return (-c._c)/c._b;
+}
--- solver.hpp
+++ solver.hpp
@@ -6,72 +6,71 @@
 #include <complex>
 using namespace std;
 
+static complex<double> ZERO_COMPLEX (0,0);
+
 namespace solver{
-////    class Function;
-//    class RealVariable;
-//    class ComplexVariable;
-class RealVariable{
-    double _re;
-public:
-    RealVariable(){}
-    RealVariable(double re){
-        _re=re;
-    }
-    friend RealVariable& operator+ (double, RealVariable&);
-    friend RealVariable& operator+ (RealVariable&, double);
-    friend RealVariable& operator+ (RealVariable& , RealVariable&);
-    friend RealVariable& operator- (double, RealVariable&);
-    friend RealVariable& operator- (RealVariable&, double);
-    friend RealVariable& operator- (RealVariable&, RealVariable&);
-    friend RealVariable& operator/ (double, RealVariable&);
-    friend RealVariable& operator/ (RealVariable&, double);
-    friend RealVariable& operator/ (RealVariable&, RealVariable&);
-    friend RealVariable& operator* (double, RealVariable&);
-    friend RealVariable& operator* (RealVariable&, double);
-    friend RealVariable& operator* (RealVariable&, RealVariable&);
-    friend RealVariable& operator== (double, RealVariable&);
-    friend RealVariable& operator== (RealVariable&, double);
-    friend RealVariable& operator== (RealVariable&, RealVariable&);
-    friend RealVariable& operator^ (RealVariable&, double);
-};
-
-class ComplexVariable{
-    double _re;
-    complex<double> _im;
-public:
-    ComplexVariable(){}
-    ComplexVariable(double re, complex<double> im){
-        _re=re;
-        _im=im;
-    }
-    friend ComplexVariable& operator+ (ComplexVariable& a, double b);
-    friend ComplexVariable& operator+ (double a, ComplexVariable& b);
-    friend ComplexVariable& operator+ (ComplexVariable& a, complex<double> b);
-    friend ComplexVariable& operator+ (complex<double> a, ComplexVariable& b);
-    friend ComplexVariable& operator+ (ComplexVariable& a, ComplexVariable& b);
-    friend ComplexVariable& operator^ (ComplexVariable& f, double a);
-    friend ComplexVariable& operator== (double a, ComplexVariable& b);
-    friend ComplexVariable& operator== (ComplexVariable& a, double b);
-    friend ComplexVariable& operator== (ComplexVariable& a, complex<double> b);
-    friend ComplexVariable& operator== (complex<double> a, ComplexVariable& b);
-    friend ComplexVariable& operator== (ComplexVariable& a, ComplexVariable& b);
-    friend ComplexVariable& operator* (double a, ComplexVariable& b);
-    friend ComplexVariable& operator* (ComplexVariable& a, double b);
-    friend ComplexVariable& operator* (ComplexVariable& a, complex<double> b);
-    friend ComplexVariable& operator* (complex<double> a, ComplexVariable& b);
-    friend ComplexVariable& operator* (ComplexVariable& a, ComplexVariable& b);
-    friend ComplexVariable& operator/ (double a, ComplexVariable& b);
-    friend ComplexVariable& operator/ (ComplexVariable& a, double b);
-    friend ComplexVariable& operator/ (ComplexVariable& a, ComplexVariable& b);
-    friend ComplexVariable& operator/ (ComplexVariable& a, complex<double> b);
-    friend ComplexVariable& operator/ (complex<double> a, ComplexVariable& b);
-    friend ComplexVariable& operator- (double a, ComplexVariable& b);
-    friend ComplexVariable& operator- (ComplexVariable& a, double b);
-    friend ComplexVariable& operator- (ComplexVariable& a, ComplexVariable& b);
-    friend ComplexVariable& operator- (ComplexVariable& a, complex<double> b);
-    friend ComplexVariable& operator- (complex<double> a, ComplexVariable& b);
-};
-
+    class RealVariable;
+    class ComplexVariable;
     double solve(RealVariable r);
     complex<double> solve(ComplexVariable c);
 }
+
+class solver::RealVariable{
+    double _a,_b,_c;
+public:
+    //Constructors & explicit
+    RealVariable ():_a(0),_b(1),_c(0){}
+    RealVariable (double d):RealVariable(0,0,d){}
+    RealVariable (double a,double b,double c):_a(a),_b(b),_c(c){}
+
+    friend RealVariable operator+ (const RealVariable& , const RealVariable&);
+    friend RealVariable operator- (const RealVariable&, const RealVariable&);
+    friend RealVariable operator/ (const RealVariable&, const RealVariable&);
+    friend RealVariable operator* (const RealVariable&, const RealVariable&);
+    friend RealVariable operator^ (const RealVariable&, double);
+
+    RealVariable operator== (const RealVariable& r2){
+        return RealVariable(
+        (this->_a!=0 && r2._a!=0) ?this->_a-r2._a : (r2._a!=0 ? -r2._a:this->_a),
+        (this->_b!=0 && r2._b!=0) ? this->_b-r2._b : (r2._b!=0 ? -r2._b:this->_b),
+        this->_c-r2._c);
+    }
+
+     friend ostream& operator<< (ostream& os, const RealVariable& r) {
+         os << r._a << "x^2+" << r._b << "x+" << r._c;
+         return os;
+     }
+    friend double solve(RealVariable r);
+};
+
+class solver::ComplexVariable{
+    complex<double> _a,_b,_c;
+public:
+    //Constructors & explicit
+    ComplexVariable():_a(0),_b(1,0),_c(0){}
+    ComplexVariable (double& d):ComplexVariable(complex<double>(d,0)){}
+    ComplexVariable (const complex<double>& d):_a(0),_b(0),_c(d){}
+    ComplexVariable (const complex<double>& a,const complex<double>& b,const complex<double>& c):_a(a),_b(b),_c(c){}
+
+    friend ComplexVariable operator+ (const ComplexVariable& a,const ComplexVariable& b);
+    friend ComplexVariable operator* (const ComplexVariable& a, const ComplexVariable& b);
+    friend ComplexVariable operator/ (const ComplexVariable& a,const ComplexVariable& b);
+    friend ComplexVariable operator- (const ComplexVariable& a,const ComplexVariable& b);
+    friend ComplexVariable operator^ (const ComplexVariable& f, double a);
+//    friend ComplexVariable operator+ (ComplexVariable& a, complex<double> b);
+    ComplexVariable operator== (const ComplexVariable& c2){
+        return ComplexVariable(
+        (this->_a!=ZERO_COMPLEX && c2._a!=ZERO_COMPLEX) ?this->_a-c2._a : (c2._a!=ZERO_COMPLEX ? -c2._a:this->_a),
+        (this->_b!=ZERO_COMPLEX && c2._b!=ZERO_COMPLEX) ? this->_b-c2._b : (c2._b!=ZERO_COMPLEX ? -c2._b:this->_b),
+        this->_c-c2._c);
+    }
+
+    friend ostream& operator<< (ostream& os, const ComplexVariable& r) {
+        os << r._a << "x^2+" << r._b << "x+" << r._c;
+        return os;
+    }
+
+    friend complex<double> solve(ComplexVariable c);
+};
+
+
